e9f737455610e1a5d38c1bbfde20f7d543caac9f
Kalin Todorov <kalin104@abv.bg>
2021-10-20 04:06:56 +0300

    main

--- components/Players/Players.js
+++ components/Players/Players.js
@@ -15,18 +15,14 @@ export default function Players() {
   });
 
   return (
-    <div
-      className="bg-white rounded-2xl  mt-4  border-gray-200 \
-     border w-11/12 m-auto max-w-4xl "
-    >
-      {/* Player Filters */}
-      <div className="flex flex-wrap relative ">
+    <>
+      <div className="flex flex-wrap relative justify-center">
         <div
-          className="rounded-md p-4 inline-table table-fixed bg-gray-100 m-3 sm:w-filter \
-        w-full"
+          className="rounded-md p-4 inline-table table-fixed bg-white border mt-1 md:w-filter \
+        w-11/12 border-gray-200  "
         >
           <div className="table-cell ">
-            <div className="relative m-2 bg-white rounded-md">
+            <div className="relative bg-white rounded-md">
               <select
                 className="relative z-1 rounded-t-lg w-full border-b-2 h-8 border-red-400 \
               before:z-0 block focus:border-0"
@@ -41,47 +37,55 @@ export default function Players() {
           </div>
         </div>
 
-        <div className="sm:w-filter rounded-md p-4 m-3 mt-0 sm:mt-3 inline-table table-fixed bg-gray-100 ml-3 w-full">
+        <div
+          className="md:w-filter rounded-md p-4 sm:mt-1 \
+         inline-table table-fixed bg-white ml-1 w-11/12 border border-gray-200 mt-1 "
+        >
           <div className="flex justify-around">
             <button
               type="button"
-              className="w-28 h-10 bg-white text-lg rounded-md"
+              className="w-28 h-10 bg-red-300 text-lg rounded-md"
             >
               Мъже
             </button>
 
             <button
               type="button"
-              className="w-28 bg-white rounded-md  text-lg leading-4"
+              className="w-28 bg-red-300 rounded-md  text-lg leading-4"
             >
               Жени
             </button>
           </div>
         </div>
       </div>
-      <div className="selectors">
-        <div className="match-filters">
-          <div className="filter">
-            <div className="select-wp">
-              <div className="autocomplete">
-                <p className="clear-filter" style={{ display: 'none' }}>
-                  <span className="fa fa-trash-o"></span>
-                </p>
-                <input
-                  type="text"
-                  placeholder="(Име)"
-                  className="form-control input-sm"
-                />
-                <div
-                  className="autocomplete-suggestions"
-                  style={{ display: 'none' }}
-                ></div>
-              </div>
+      <div className="flex flex-wrap relative justify-center">
+        <div
+          className="rounded-md p-4 inline-table table-fixed bg-white mt-1 mb-0 md:mb-1 md:w-filter \
+        w-11/12 border-gray-200 border "
+        >
+          <div className="select-wp">
+            <div className="autocomplete">
+              <p className="clear-filter" style={{ display: 'none' }}>
+                <span className="fa fa-trash-o"></span>
+              </p>
+              <input
+                type="text"
+                placeholder="(Име)"
+                className="relative z-1 rounded-t-lg w-full border-b-2 h-8 border-red-400 \
+                before:z-0 block focus:border-0"
+              />
+              <div
+                className="autocomplete-suggestions"
+                style={{ display: 'none' }}
+              ></div>
             </div>
           </div>
         </div>
 
-        <div className="match-filters">
+        <div
+          className="border-gray-200 border rounded-md p-4 inline-table table-fixed
+           bg-white m-1 md:w-filter w-11/12"
+        >
           <div className="filter">
             <div className="select-wp">
               <div className="autocomplete">
@@ -92,21 +96,29 @@ export default function Players() {
                   type="text"
                   value=""
                   placeholder="(Град)"
-                  className="form-control input-sm "
+                  className="relative z-1 rounded-t-lg w-full border-b-2 h-8 border-red-400 \
+                  before:z-0 block focus:border-0"
                 />
               </div>
             </div>
           </div>
         </div>
       </div>
-      {/* Map Players*/}
-      <div className="">
-        <div className="p-2 w-full">
-          {Player.map((item) => (
-            <Item key={`${item.id}`} {...item} />
-          ))}
+      <div
+        className="bg-white rounded-2xl  mt-4  border-gray-200 \
+     border w-11/12 m-auto max-w-4xl "
+      >
+        {/* Player Filters */}
+
+        {/* Map Players*/}
+        <div className="">
+          <div className="p-2 w-full">
+            {Player.map((item) => (
+              <Item key={`${item.id}`} {...item} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
